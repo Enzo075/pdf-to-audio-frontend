3580a30c3075e5a41405be9c4d42d7047f65a855
Enzo075 <Enzoklai07@gmail.com>
2025-12-19 16:50:54 -0300

    save

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const API_BASE_URL: string =
+  import.meta.env.VITE_API_URL || "http://localhost:3001";
+export const MAX_FILE_SIZE: number = 10 * 1024 * 1024;
+export const MAX_TEXT_LENGTH: number = 5000;
